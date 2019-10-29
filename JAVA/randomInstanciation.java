cd1434e0bb0dadf62b097745bb240150d7f1b701
Boris BELLOC <37147299+borisBelloc@users.noreply.github.com>
2019-10-29 09:45:01 +0100

    Create randomInstanciation.java

---
+++ JAVA/randomInstanciation.java
@@ -0,0 +1,27 @@
+public class Main {
+
+	public static void main(String[] args) {
+
+
+      
+     // Tab instanciantion
+     
+     int[] myTab = {2,3,5};
+
+
+    // Collection instanciation with value.
+    // #1 one lane
+    List<Integer> myList = Arrays.asList(3, 4, 12);
+
+    // #2
+    List<Integer> list = new ArrayList<>();
+		list.add(1);
+		list.add(3);
+
+
+
+
+
+	}
+
+}
