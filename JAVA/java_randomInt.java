37501bfff1914620f45e5ab4ef3d467d74b045b3
Boris BELLOC <37147299+borisBelloc@users.noreply.github.com>
2019-11-01 16:02:40 +0100

    added 2 methodes for random int

--- JAVA/java_randomInt.java
+++ JAVA/java_randomInt.java
@@ -1,13 +1,39 @@
 import java.util.concurrent.ThreadLocalRandom;
 
-public class FigureUtil {
+public class RandomUtil {
 	
+	
+	/**
+	 * Generates a random integer from 0 (inclusive) to vMax (inclusive)
+	 * @param vMax
+	 * @return int
+	 */
+	public static int getRandomInt(int vMax ) {
+		return new Random().nextInt(vMax +1);
+	}
+	
+	/**
+	 * Generates a random integer between vMin (inclusive) and vMax (inclusive).
+	 * @param vMin
+	 * @param vMax
+	 * @return int
+	 */
+	public static int getRandomIntInRange(int vMin, int vMax) {
+		if (vMin >= vMax) {
+			throw new IllegalArgumentException("max must be greater than min");
+		}
+		return new Random().nextInt((vMax) + 1) + vMin;
+	}
+	
+	
+	
+	/** ------------------------------
+	* Test; not finished ;
+	*/
 	private void getRandomInt() {
-		// nextInt is normally exclusive of the top value,
-		// so add 1 to make it inclusive
 		// sauce: https://stackoverflow.com/a/363692/9552861
 
-    // TODO: Change min and max to desired values
+    	// TODO: Change min and max to desired values
 		int randomNum = ThreadLocalRandom.current().nextInt(min, max + 1);
 		System.out.println(randomNum);
 	}
