7be245134f8320b5259ecc6f83f9062268965a48
Boris BELLOC <37147299+borisBelloc@users.noreply.github.com>
2019-11-23 17:30:26 +0100

    capitalizeWord()

---
+++ JAVA/UtilsString.java
@@ -0,0 +1,10 @@
+package utils;
+
+public class Utils {
+	
+
+	public static String capitalizeWord(String word) {
+		return word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase();
+	}
+
+}
